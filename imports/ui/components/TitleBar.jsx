0f5d1246b145088310d03de422829cd83803bc21
jist <jistjoalal@gmail.com>
2019-03-08 05:26:23 -0800

    move github link in titlebar

--- imports/ui/components/TitleBar.jsx
+++ imports/ui/components/TitleBar.jsx
@@ -3,14 +3,24 @@ import { Link } from 'react-router-dom';
 
 const GH = 'github';
 
-const TitleLink = ({ title }) =>
+const TitleLinks = ({ title }) =>
   title == "IDW" ?
-    <Link className="btn btn-secondary" to="/channels">
-      All Channels
-    </Link>
-  : <Link className="btn btn-secondary" to="/">
-      Home
-    </Link>
+    <div>
+      <Link className="btn btn-secondary" to="/channels">
+        All Channels
+      </Link>
+      <a target="blank"
+        className="text-monospace btn btn-secondary ml-2"
+        href="https://github.com/jistjoalal/youtube-agg"
+      >
+        { GH }
+      </a>
+    </div>
+  : <div>
+      <Link className="btn btn-secondary" to="/">
+        Home
+      </Link>
+    </div>
 
 export default TitleBar = ({ children, title }) =>
   <nav className="TitleBar navbar bg-secondary text-dark border-bottom d-flex flex-column">
@@ -19,18 +29,8 @@ export default TitleBar = ({ children, title }) =>
 
         <h1 className="serif font-weight-bold m-0">{title}</h1>
 
-        <div>
-          <TitleLink title={title} />
-          
-          <a target="blank"
-            className="text-monospace btn btn-secondary ml-2"
-            href="https://github.com/jistjoalal/youtube-agg"
-          >
-            { GH }
-          </a>
+        <TitleLinks title={title} />
 
-        </div>
-        
     </div>
    
     {children &&
