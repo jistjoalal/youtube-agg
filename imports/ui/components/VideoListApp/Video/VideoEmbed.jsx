8b7451f7b7d888409ba5572561af46c9609274fb
jist <jistjoalal@gmail.com>
2019-03-11 10:29:10 -0700

    loading icon for videoplayer

--- imports/ui/components/VideoListApp/Video/VideoEmbed.jsx
+++ imports/ui/components/VideoListApp/Video/VideoEmbed.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { FaSpinner } from 'react-icons/fa';
+
+const LoadingIcon = _ =>
+  <FaSpinner className="LoadingIcon" />
 
 export default VideoEmbed = ({ _id }) =>
   <div className="VideoEmbed shadow">
+
+    <div className="VideoEmbed__loading">
+      <LoadingIcon />
+    </div>
+
     <iframe
       className="VideoEmbed__iframe"
       src={`https://www.youtube.com/embed/${_id}?autoplay=1`}
