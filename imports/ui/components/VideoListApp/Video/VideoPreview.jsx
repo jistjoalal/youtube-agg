727db725b87be322fcd83912ab30fbc945c9fb5e
jist <jistjoalal@gmail.com>
2019-03-11 08:22:17 -0700

    a little better styles

--- imports/ui/components/VideoListApp/Video/VideoPreview.jsx
+++ imports/ui/components/VideoListApp/Video/VideoPreview.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import VideoDescription from './VideoDescription';
 
 export default VideoPreview = ({ video, open }) =>
-  <div className="VideoPreview">
+  <div className="VideoPreview p-2">
   
     <a href="" onClick={e => { e.preventDefault(); open(); }}>
       <img src={video.img} className="VideoPreview__thumb" />
