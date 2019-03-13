bc236719508ac4d8ec7910b0f64b201ad9d31e47
jist <jistjoalal@gmail.com>
2019-03-13 11:00:26 -0700

    refactor viewspersec calculation

--- imports/api/scraper/scraper.js
+++ imports/api/scraper/scraper.js
@@ -5,11 +5,13 @@ import Channels from '../channels';
 import { parseVideos, parseChannel } from './parser';
 import CHANNEL_IDS from './channelIds';
 
-export default scrape = _ => {
-  CHANNEL_IDS.forEach(scrapeChannel);
-}
 
-export const scrapeChannel = _id => {
+// Scrapes all channels
+export default scrape = _ => CHANNEL_IDS.forEach(scrapeChannel);
+
+// parse a youtube channel's 'videos' page
+// save scraped data to db
+const scrapeChannel = _id => {
   const reqUrl = `https://www.youtube.com/channel/${_id}/videos`;
   axios(reqUrl)
     .then(({ data }) => {
@@ -19,24 +21,11 @@ export const scrapeChannel = _id => {
 
       parseVideos(data, channel).forEach(updateVideo);
 
-      updateViewGrowth();
-      
     })
     .catch(err => console.log(err))
 }
 
-const updateViewGrowth = _ => {
-  Videos.find({}).fetch().forEach(video => {
-    const deltaTime = (Date.now() - video.postedTime) / 1000;
-    const viewsPerSec = video.viewCount / deltaTime;
-    const vid = {
-      ...video,
-      viewsPerSec,
-    };
-    Videos.update(video, vid);
-  });
-}
-
+// submit results of scraping to Channels collection
 const updateChannel = channel => {
   const q = { _id: channel._id };
   if (Channels.findOne(q)) {
@@ -45,8 +34,10 @@ const updateChannel = channel => {
   return Channels.insert(channel);
 }
 
+// submit results of scraping to Videos collection
 const updateVideo = vid => {
   if (!vid) return;
+
   const q = { _id: vid._id };
   const cur = Videos.findOne(q);
 
@@ -56,13 +47,26 @@ const updateVideo = vid => {
     // since it's a weird reverse timeago calculation
     // it's more accurate the less it's updated
     vid.postedTime = cur.postedTime;
-
+    
+    vid.viewsPerSec = viewsPerSec(vid)
     return Videos.update(q, vid);
   }
 
   // video is new
   else {
+    vid.viewsPerSec = viewsPerSec(vid);
     return Videos.insert(vid);
   }
+}
+
+/**
+ * Calculate views per second
+ * - Must be run outside parser
+ * - calculation depends on accurate postedTime which we do not reparse
+ * - (see updateVideo)
+ */
+const viewsPerSec = ({ postedTime, viewCount }) => {
+  const deltaTime = (Date.now() - postedTime) / 1000;
+  return viewCount / deltaTime;
 }
 
