52905c74712aea2cddf3e789ed11884311dee09f
jist <jistjoalal@gmail.com>
2019-03-16 11:33:14 -0700

    remove bootstrap deps - no bootstrap js code used

--- client/main.jsx
+++ client/main.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-
 import Routes from '/imports/Routes';
 
 Meteor.startup(() => {
